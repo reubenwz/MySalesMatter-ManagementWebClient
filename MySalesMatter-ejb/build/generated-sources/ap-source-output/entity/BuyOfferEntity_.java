cabfe7e97c53f6128b10501030e8421ca0491653
rxchtan <r.tan3009@hotmail.com>
2021-03-31 17:22:10 +0800

    added web-inf

--- MySalesMatter-ejb/build/generated-sources/ap-source-output/entity/BuyOfferEntity_.java
+++ MySalesMatter-ejb/build/generated-sources/ap-source-output/entity/BuyOfferEntity_.java
@@ -3,7 +3,7 @@ package entity;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-03-31T17:01:21")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-03-31T17:19:01")
 @StaticMetamodel(BuyOfferEntity.class)
 public class BuyOfferEntity_ extends OfferEntity_ {
 
--- MySalesMatter-ejb/build/generated-sources/ap-source-output/entity/MessageEntity_.java
+++ MySalesMatter-ejb/build/generated-sources/ap-source-output/entity/MessageEntity_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-03-31T17:01:21")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-03-31T17:19:01")
 @StaticMetamodel(MessageEntity.class)
 public class MessageEntity_ { 
 
