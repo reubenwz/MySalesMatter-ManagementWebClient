df067f60a92469491f06a6e2eb95c160bfd7fc67
Sylvia31082000 <e0439276@u.nus.edu>
2021-04-10 13:44:38 +0800

    edited web services

--- MySalesMatter-ejb/build/generated-sources/ap-source-output/entity/BuyOfferEntity_.java
+++ MySalesMatter-ejb/build/generated-sources/ap-source-output/entity/BuyOfferEntity_.java
@@ -3,7 +3,7 @@ package entity;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-10T12:42:47")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-10T13:35:28")
 @StaticMetamodel(BuyOfferEntity.class)
 public class BuyOfferEntity_ extends OfferEntity_ {
 
