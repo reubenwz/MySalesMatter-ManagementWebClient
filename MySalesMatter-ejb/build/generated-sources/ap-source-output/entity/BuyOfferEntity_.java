7946b160fe440342d73b3a2184a1fb7d2ce0b65a
rxchtan <r.tan3009@hotmail.com>
2021-04-12 09:06:45 +0800

    merging

--- MySalesMatter-ejb/build/generated-sources/ap-source-output/entity/BuyOfferEntity_.java
+++ MySalesMatter-ejb/build/generated-sources/ap-source-output/entity/BuyOfferEntity_.java
@@ -3,7 +3,7 @@ package entity;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-11T10:42:47")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-11T23:10:54")
 @StaticMetamodel(BuyOfferEntity.class)
 public class BuyOfferEntity_ extends OfferEntity_ {
 
--- MySalesMatter-ejb/build/generated-sources/ap-source-output/entity/RentalOfferEntity_.java
+++ MySalesMatter-ejb/build/generated-sources/ap-source-output/entity/RentalOfferEntity_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-11T10:42:47")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-11T23:10:54")
 @StaticMetamodel(RentalOfferEntity.class)
 public class RentalOfferEntity_ extends OfferEntity_ {
 
