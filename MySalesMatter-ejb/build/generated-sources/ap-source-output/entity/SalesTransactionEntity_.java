46ea01ee8de2493a27fb5b02303b25095f97424f
Reuben Ang <e0407386@u.nus.edu>
2021-04-13 21:33:14 +0800

    edit loans and purchase

--- MySalesMatter-ejb/build/generated-sources/ap-source-output/entity/SalesTransactionEntity_.java
+++ MySalesMatter-ejb/build/generated-sources/ap-source-output/entity/SalesTransactionEntity_.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-13T16:29:53")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-13T18:30:42")
 @StaticMetamodel(SalesTransactionEntity.class)
 public class SalesTransactionEntity_ { 
 
